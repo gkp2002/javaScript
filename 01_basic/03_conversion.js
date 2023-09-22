34476cd003ae4dec9e0b88a439ec572cc7da945e
gkp2002 <123437562+gkp2002@users.noreply.github.com>
2023-09-22 21:38:01 +0000

    operator

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,8 +1,8 @@
 let score= true
-console.log(typeof(score))
+// console.log(typeof(score))
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 /*
  conversion
@@ -13,9 +13,9 @@ console.log(valueInNumber)
 
   let isloggedin=0;
   let booleanisLogggedIn=Boolean(isloggedin)
-  console.log(isloggedin);
-  console.log(typeof booleanisLogggedIn);
-  console.log(booleanisLogggedIn)
+//   console.log(isloggedin);
+//   console.log(typeof booleanisLogggedIn);
+//   console.log(booleanisLogggedIn)
   /*
   1=>true 0=>false
   " "=> false
@@ -25,5 +25,46 @@ console.log(valueInNumber)
 
    let num = 64
    let strings = String(num)
-   console.log(typeof strings)
-   console.log(strings)
+//    console.log(typeof strings)
+//    console.log(strings)
+
+// *******************operation*********************
+let value=3
+let neg = - value
+//console.log(neg);
+
+// **************ARithmetic**********
+/*console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3);
+console.log(6/3);
+console.log(4%3);
+*/
+
+/*
+let str = "gajanan"
+let str1= "kumar"
+console.log(str+str1)
+console.log("1"+2);
+console.log(2+"1");
+console.log("1"+ 2 + 2);
+console.log(2+2+"1");
+
+console.log(3+2*5%3); // not in use
+*/
+// console.log(true)
+
+
+
+// let num1,num2,num3
+// num1=num2=num3=3+3
+// console.table([num1,num1,num3])
+
+
+let gamecount=2;
+gamecount++;
+console.log(gamecount)
+++gamecount
+console.log(gamecount);
+
