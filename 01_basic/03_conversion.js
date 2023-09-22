8d4a25de819cee224c5e84c35560073d83153edd
gkp2002 <123437562+gkp2002@users.noreply.github.com>
2023-09-22 21:15:45 +0000

    conversion datatype

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,29 @@
+let score= true
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+/*
+ conversion
+  "33"=.33
+  "33abc"=> NaN
+  true=>1; false =>0
+  */
+
+  let isloggedin=0;
+  let booleanisLogggedIn=Boolean(isloggedin)
+  console.log(isloggedin);
+  console.log(typeof booleanisLogggedIn);
+  console.log(booleanisLogggedIn)
+  /*
+  1=>true 0=>false
+  " "=> false
+  "gajanan"=> true
+   
+  */
+
+   let num = 64
+   let strings = String(num)
+   console.log(typeof strings)
+   console.log(strings)
