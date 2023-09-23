1d7e12efaff195a1827aacc75dfec96709afbfab
gkp2002 <123437562+gkp2002@users.noreply.github.com>
2023-09-23 13:24:57 +0000

    datatype summery

--- 01_basic/04_comparison.js
+++ 01_basic/04_comparison.js
@@ -19,3 +19,34 @@
 // console.log(undefined>=0);
 
 console.log("2"===2) //comprison value with datatype
+//**************************Data type Summary*******************************
+
+// primitive data type
+// 7type=> string,number,boolean,null,undefined,symbol,Bigint
+const score = 200
+const scorevalue = 201.3
+const value=false
+const nullvalue=null
+let undefin;
+
+const value1 = Symbol('1000')
+const value2 = Symbol('1000')
+
+
+
+
+
+//Refrence type or Non primitive type
+ // Arrats, Objects,functions
+  
+const heros =["gajanan","shivam","prem"];
+let object = {
+    name:"gajanan",
+    age:19
+}
+const myfunction=function(){
+    console.log("hello gajanna")
+}
+
+console.log(myfunction)
+console.log(typeof nullvalue);
