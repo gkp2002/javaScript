872a561fab0f8633be08aaae1d3783fd167ded4e
gkp2002 <123437562+gkp2002@users.noreply.github.com>
2023-09-23 14:33:08 +0000

    math and Num

---
+++ 01_basic/06_Nums_Math.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 123.8777
+// console.log(otherNumber.toPrecision(4))
+// const hunderds = 100000
+// console.log(hunderds.toLocaleString('en-IN'));
+// console(Number.MAX_VALUE)
+
+
+
+//************** Math +++++++++++++++++++++
+
+
+// console.log(Math.PI)
+// console.log(Math.round(4.5))
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(4.11))
+// console.log(Math.floor(3.44))
+// console.log(Math.sqrt(84));
+// console.log(Math.min(4,3,55,6,7))
+// console.log(Math.max(2,33,4,66,7,8,1))
+
+console.log(Math.random()) //return number between 0 to 1
+console.log(Math.floor(Math.random()*10)+1 )
+const min=10
+const max =20
+
+console.log(Math.floor(Math.random()*(max-min +1))+min)
